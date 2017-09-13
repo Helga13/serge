dc3d88b6ad52b55c3048c686b606309f649c6fbb
Helga Helen <oliatrubina@yandex.ru>
2017-09-13 16:34:43 +0300

    main page compleet

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,50 @@ $(document).ready(function () {
     slidesToShow: 1
   });
   
+  // video 
+  
+  var myPlayer = $('.video');  
+  var w = parseInt($('.video').css('width'));
+  var h = parseInt($('.video').css('height')) ;
+
+	$('video').mediaelementplayer({
+		alwaysShowControls: true,
+		videoVolume: 'horizontal',
+		defaultVideoWidth: w ,
+		defaultVideoHeight: h,
+		// if set, overrides <video width>
+		videoWidth: -1,
+		// if set, overrides <video height>
+		videoHeight: -1,
+		enableAutosize: true,
+		videoVolume: 'vertical'
+	});
+  
+  //menu
+  
+  var body = $('body');
+  
+  $('.js-toggle-menu').on('click', function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('is-active')) {
+      $('#js-menu').fadeOut(100);
+      $(this).removeClass('is-active');
+      body.removeClass('no-scroll');
+    }
+    else {
+      $('#js-menu').fadeIn(100);
+      $(this).addClass('is-active');
+      body.toggleClass('no-scroll');
+    }
+    return false;
+  });
+  
+  $('.popup_close').on('click', function (e) {
+    e.preventDefault();
+    $('#js-menu').fadeOut(100);
+    $('.js-toggle-menu, .js-search, .js-popupCall').removeClass('is-active');
+    body.removeClass("no-scroll");
+  });
+  
+  
 })
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,6 @@
 //= lib/head.js
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= lib/slick.min.js
+//= lib/mediaelement-and-player.min.js
 
 //= app.js
